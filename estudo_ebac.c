65b198aadb8884a35c9e97c9992ba3c9fcd58aa9
heberrossi <137964988+heberrossi@users.noreply.github.com>
2023-06-27 22:16:17 -0300

    Opção de saída e Continuação de Registro
    Incluso no sistema a opção de saída pro usuário e de continuação de registro de nomes quando acaba de registrar um novo usuário

--- estudo_ebac.c
+++ estudo_ebac.c
@@ -60,6 +60,30 @@ int registro() //função responsável por cadastrar os usuários no sisterma
 		fprintf (file, cargo);
 		fclose(file);
 		
+		system("cls");
+		
+		int opcao=0;
+		
+		printf("\nDeseja voltar ao menu ou registrar mais usuários?\n\n");
+		printf("\t1 - Registrar mais nomes\n");
+		printf("\t2 - Voltar ao menu\n");
+		printf("\nOpção: ");
+		
+		scanf("%d", &opcao);
+		
+		switch(opcao)
+   		{
+    		case 1:
+    		system("cls");
+    		registro();
+			break;
+			   
+			case 2:
+			system("cls");
+			printf("\nVocê escolheu voltar ao menu\n\n");
+			break;
+			
+		}
 		system("pause");
 	
 }
@@ -146,6 +170,7 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar os nomes\n");
    		printf("\t3 - Deletar os nomes\n\n"); 
+   		printf("\t4 - Sair do sistema\n\n"); 
    		printf("Opção: "); //final do menu
    
     	scanf("%d", &opcao); //armazenando a escolha do usuário
@@ -165,6 +190,11 @@ int main()
 			case 3:
     		deletar();
 			break;	 
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
+			break;
 			
 			default:
     		printf("não existe essa opção\n\n");
